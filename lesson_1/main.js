17d843811e593cd14ccd640d722ca1c66f9891e4
Huy IT <leanhhuy.it@gmail.com>
2025-03-23 14:18:28 +0700

    Update code

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -67,11 +67,40 @@
 //     console.log('Dieu kien sai');   
 // }
 
-//16
+// //16
+// var a = 1;
+// var b = 2;
+// var c = 3;
+
+// if (!(a > 0)) {
+//     console.log('DIEU KIEN DUNG!');
+// }
+
+//17
+// var myObject = {
+//     name: 'Anh Huy',
+//     age: 28,
+//     address: 'Ho Chi Minh',
+//     myFunction: function () {
+
+//     }
+// };
+
+// var myArray = [
+//     'Javascrip',
+//     'PHP',
+//     'Ruby'
+// ];
+// console.log(myArray)
+
+//18,19
+
 var a = 1;
 var b = 2;
-var c = 3;
 
-if (!(a > 0)) {
-    console.log('DIEU KIEN DUNG!');
+var result = 'A' || 'B' || undefined || 'D';
+if(result) {
+    console.log('DIEU KIEN DUNG');
+} else {
+    console.log('DIEU KIEN SAI');
 }
