45205ec4d2c4afee7aea13316aad8a4e71ee5080
Huy IT <leanhhuy.it@gmail.com>
2025-03-23 15:31:11 +0700

    Update bai 21

---
+++ lesson_1/bai21.js
@@ -0,0 +1,12 @@
+var myString = 'Hoc JS tai F8!';
+//console.log(myString.length);
+//console.log(myString.slice(0))
+//console.log(myString.replace('JS', 'Javascrip'))
+//5.
+// console.log(myString.toLocaleUpperCase())
+// console.log(myString.toLocaleLowerCase())
+// console.log(myString.trim())
+// var languages = 'Javascrip, PHP, Ruby';
+// console.log(languages.split(', '))
+const myString2 = 'Anh Huy';
+console.log(myString2[10])
