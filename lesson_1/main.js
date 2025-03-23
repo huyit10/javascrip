8ac0e85815583aad4b77e0b944bb7f8eea1d52ca
Huy IT <leanhhuy.it@gmail.com>
2025-03-23 13:20:16 +0700

    Initial commit

---
+++ lesson_1/main.js
@@ -0,0 +1,77 @@
+
+// var fullName = "Le Anh Huy";
+// var age = 28;
+// alert(fullName);
+// alert(age);
+
+// prompt('xac nhan du tuoi!');
+// setInterval(function() {
+//     console.log('Day la log' + Marth.random())
+// }, 1000
+
+// console.log(a)
+// var a = 1;
+// var b = 2; 
+
+// if (a > 0 && b>0) {
+//     alert ('a & b lon hon 0')
+// }
+
+// var a = 10;
+// var b = 2;
+
+// var c = a % b;
+// console.log(c);
+
+
+// var a = 6;
+
+// var output = a++;
+
+// console.log('output: ', output);
+
+// console.log('a: ', a);
+
+// var a = 1;
+
+// a = a + 2;
+
+// console.log(a);
+
+
+
+// var name = 'Anh';
+// name += 'Huy';
+// console.log(name);
+
+// var a = 1;
+// var b = 2;
+
+// if (a >= b) {
+// console.log('Dieu kien dung!');
+// } else {
+// console.log ('Dieu kien sai!');
+// }
+
+//14
+// var a = 1;
+// var b = 2;
+// var isSuccess = a > b;
+
+// console.log(isSuccess);
+
+// //15
+// if (true) {
+//     console.log('Dieu kien dung');
+// } else {
+//     console.log('Dieu kien sai');   
+// }
+
+//16
+var a = 1;
+var b = 2;
+var c = 3;
+
+if (!(a > 0)) {
+    console.log('DIEU KIEN DUNG!');
+}
