eaab0e86cedbd26612ea7f1d6c14d91f4e520962
huyit10 <leanhhuy.it@gmail.com>
2025-03-26 17:38:10 +0700

    Update 26-3

---
+++ lesson_1/b63-64-dom-api.js
@@ -0,0 +1,14 @@
+//html dom
+//co 3 thanh phan
+/**
+ * 1. Element
+ * 2 . Attribute
+ * 3 . Text
+ */
+// -------------------------
+// Javascrip: Browser | Server (NodeJS)
+// Application programming interface
+// Browser: HTML -> DOM -> DOM API
+document.write('HELLO GUYS!');
+document.write('HELLO GUYS!');
+document.write('HELLO GUYS!');
